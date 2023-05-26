d3f9437c9f3be9d4c338a000828b5326bb8d7abe
estarrMill <133377945+estarrMill@users.noreply.github.com>
2023-05-26 15:23:55 +0100

    Update BankUI.java

--- BankUI.java/src/UI/BankUI.java
+++ BankUI.java/src/UI/BankUI.java
@@ -1,5 +1,3 @@
-package UI;
-
 
 
 import javax.swing.*;
@@ -7,140 +5,136 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class BankUI {
-    JFrame bankFrame = new JFrame("HELLO!!");
-    JLabel bank_label = new JLabel("WELCOME TO JD BANK");
-    JTextArea bank_area = new JTextArea(" Dear customer which account would you like to use? ");
-    JButton bank_button = new JButton("CURRENT ACCOUNT");
-    JButton getBank_button = new JButton("SAVINGS ACCOUNT");
+    private final int DEFAULT_PIN = 5555;
 
+    private JFrame bankFrame;
+    private JLabel bank_label;
+    private JTextArea bank_area;
+    private JButton bank_button;
+    private JButton getBank_button;
 
-    JFrame CurrentFrame = new JFrame("hello");
-    JButton currentaccount = new JButton("WITHDRAWAL");
-    JButton getCurrentaccount = new JButton("DEPOSIT");
-    JTextField current_amount1 = new JTextField("AMOUNT");
-    JTextField current_amount = new JTextField("AMOUNT");
-    JTextField current_pin = new JTextField("insert PIN");
-    JFrame SavingsFrame = new JFrame("THANK YOU FOR CHOOSING JD SAVINGS ACCOUNT");
-    JLabel Savings = new JLabel("THANK YOU FOR CHOOSING JD SAVINGS ACCOUNT");
-    JButton Withdrawalbutton = new JButton("WITHDRAWAL");
-    JButton getSaving_account = new JButton("DEPOSIT");
+    private JFrame currentFrame;
+    private JButton currentAccountWithdrawalButton;
+    private JButton currentAccountDepositButton;
+    private JTextField currentAmountField;
+    private JTextField currentPinField;
 
-    JTextField Savings_account2 = new JTextField("AMOUNT");
-    JTextField Savings_pin = new JTextField( "PIN");
-    JFrame WithdrawlFrame1 = new JFrame();
-    JFrame Depositframe1 = new JFrame();
-    JFrame WithdrawalFRAME2= new JFrame();
+    private JFrame savingsFrame;
+    private JLabel savingsLabel;
+    private JButton savingsWithdrawalButton;
+    private JButton savingsDepositButton;
+    private JTextField savingsAmountField;
+    private JTextField savingsPinField;
 
-    JFrame getDepositframe2= new JFrame();
+    public BankUI() {
+        initializeUI();
+        addEventListeners();
+    }
 
+    private void initializeUI() {
+        bankFrame = new JFrame("JD BANK");
+        bankFrame.setLayout(new FlowLayout());
+        bankFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        bank_label = new JLabel("WELCOME TO JD BANK");
+        bank_area = new JTextArea("Dear customer, which account would you like to use?");
+        bank_button = new JButton("CURRENT ACCOUNT");
+        getBank_button = new JButton("SAVINGS ACCOUNT");
 
-    public void mainUI() {
         bankFrame.add(bank_label);
         bankFrame.add(bank_area);
         bankFrame.add(bank_button);
         bankFrame.add(getBank_button);
-
-
-        //NEED TO SEFlT BUTTON SIZE SO IT DONT OVERFLOW
-////    bankFrame.add(Current_account);       //NOT WORKING
-        bankFrame.setLayout(new FlowLayout(FlowLayout.TRAILING));
+        bankFrame.setSize(300, 150);
         bankFrame.setVisible(true);
-        bankFrame.setSize(200, 250);
 
-        CurrentFrame = new JFrame();
-        CurrentFrame.add(currentaccount);
-        CurrentFrame.add(getCurrentaccount);
-        CurrentFrame.setLayout(new FlowLayout(FlowLayout.TRAILING));
-        CurrentFrame.setVisible(true);
-        CurrentFrame.setSize(200, 250);
+        currentFrame = new JFrame("CURRENT ACCOUNT");
+        currentAccountWithdrawalButton = new JButton("WITHDRAWAL");
+        currentAccountDepositButton = new JButton("DEPOSIT");
+        currentAmountField = new JTextField("AMOUNT");
+        currentPinField = new JTextField("PIN");
 
+        currentFrame.setLayout(new FlowLayout());
+        currentFrame.add(currentAccountWithdrawalButton);
+        currentFrame.add(currentAccountDepositButton);
+        currentFrame.add(currentAmountField);
+        currentFrame.add(currentPinField);
+        currentFrame.setSize(300, 150);
 
+        savingsFrame = new JFrame("JD SAVINGS ACCOUNT");
+        savingsLabel = new JLabel("THANK YOU FOR CHOOSING JD SAVINGS ACCOUNT");
+        savingsWithdrawalButton = new JButton("WITHDRAWAL");
+        savingsDepositButton = new JButton("DEPOSIT");
+        savingsAmountField = new JTextField("            ");
+        savingsPinField = new JTextField("             ");
 
+        savingsFrame.setLayout(new FlowLayout());
+        savingsFrame.add(savingsLabel);
+        savingsFrame.add(savingsWithdrawalButton);
+        savingsFrame.add(savingsDepositButton);
+        savingsFrame.add(savingsAmountField);
+        savingsFrame.add(savingsPinField);
+        savingsFrame.setSize(300, 150);
+    }
 
-        WithdrawlFrame1.add(current_amount);
-
-
-
-
-        bank_button.addActionListener(e ->{
-           // CurrentFrame;
-            CurrentFrame.add(currentaccount);
-            CurrentFrame.add(getCurrentaccount);
-
-            CurrentFrame.setLayout(new FlowLayout(FlowLayout.TRAILING));
-            CurrentFrame.setVisible(true);
-            CurrentFrame.setSize(200, 250);
-
+    private void addEventListeners() {
+        bank_button.addActionListener(e -> {
+            bankFrame.setVisible(false);
+            currentFrame.setVisible(true);
         });
 
-       getBank_button.addActionListener(e ->{
-            SavingsFrame.add(SavingsFrame);
-            SavingsFrame.add(Savings);
-           SavingsFrame.add(Savings_pin);
-            SavingsFrame.setLayout(new FlowLayout(FlowLayout.TRAILING));
-            SavingsFrame.setVisible(true);
-            SavingsFrame.setSize(250, 300);
-
-
-        });
-
-        currentaccount.addActionListener(e ->{
-            WithdrawlFrame1.setSize(200, 300);
-           WithdrawlFrame1.setLayout(new FlowLayout(FlowLayout.TRAILING));
-            WithdrawlFrame1.setVisible(true);
-        });
-
-        Depositframe1.add(current_amount1);
-
-        getCurrentaccount.addActionListener(e -> {
-                    Depositframe1.setSize(200, 300);
-            Depositframe1.setLayout(new FlowLayout(FlowLayout.TRAILING));
-            Depositframe1.setVisible(true);
-                }  );
-
-        getBank_button.addActionListener(e ->{
-            SavingsFrame= new JFrame();
-            SavingsFrame.add(SavingsFrame);
-            SavingsFrame.add(Savings);
-            SavingsFrame.setLayout(new FlowLayout(FlowLayout.TRAILING));
-            SavingsFrame.setVisible(true);
-            SavingsFrame.setSize(200, 250);
-
-
-        });
-
-            SavingsFrame= new JFrame();
-            SavingsFrame.add(Withdrawalbutton);
-            SavingsFrame.add(getSaving_account);
-            SavingsFrame.setLayout(new FlowLayout(FlowLayout.TRAILING));
-        SavingsFrame.setVisible(true);
-        SavingsFrame.setSize(200, 250);
-
-        WithdrawalFRAME2.add(Savings_account2);
         getBank_button.addActionListener(e -> {
-            SavingsFrame.add(SavingsFrame);
-            SavingsFrame.add(Savings);
-
-            SavingsFrame.setLayout(new FlowLayout(FlowLayout.TRAILING));
-            SavingsFrame.setVisible(true);
-            SavingsFrame.setSize(200, 250);
-
-
-        });
-        Withdrawalbutton.addActionListener(e ->{
-            WithdrawalFRAME2.setSize(200, 300);
-            WithdrawalFRAME2.setLayout(new FlowLayout(FlowLayout.TRAILING));
-            WithdrawalFRAME2.setVisible(true);
+            bankFrame.setVisible(false);
+            savingsFrame.setVisible(true);
         });
 
+        currentAccountWithdrawalButton.addActionListener(e -> {
+            int pin = Integer.parseInt(currentPinField.getText());
+            if (pin == DEFAULT_PIN) {
+                double amount = Double.parseDouble(currentAmountField.getText());
+                // Perform withdrawal logic for current account
+                JOptionPane.showMessageDialog(null, "Withdrawal successful.");
+            } else {
+                JOptionPane.showMessageDialog(null, "Invalid PIN.");
+            }
+        });
 
-       getDepositframe2.add(Savings_account2);
-       getSaving_account.addActionListener(e -> {
+        currentAccountDepositButton.addActionListener(e -> {
+            int pin = Integer.parseInt(currentPinField.getText());
+            if (pin == DEFAULT_PIN) {
+                double amount = Double.parseDouble(currentAmountField.getText());
+                // Perform deposit logic for current account
+                JOptionPane.showMessageDialog(null, "Deposit successful.");
+            } else {
+                JOptionPane.showMessageDialog(null, "Invalid PIN.");
+            }
+        });
 
-       });
+        savingsWithdrawalButton.addActionListener(e -> {
+            int pin = Integer.parseInt(savingsPinField.getText());
+            if (pin == DEFAULT_PIN) {
+                double amount = Double.parseDouble(savingsAmountField.getText());
+                // Perform withdrawal logic for savings account
+                JOptionPane.showMessageDialog(null, "Withdrawal successful.");
+            } else {
+                JOptionPane.showMessageDialog(null, "Invalid PIN.");
+            }
+        });
+
+        savingsDepositButton.addActionListener(e -> {
+            int pin = Integer.parseInt(savingsPinField.getText());
+            if (pin == DEFAULT_PIN) {
+                double amount = Double.parseDouble(savingsAmountField.getText());
+                // Perform deposit logic for savings account
+                JOptionPane.showMessageDialog(null, "Deposit successful.");
+            } else {
+                JOptionPane.showMessageDialog(null, "Invalid PIN.");
+            }
+        });
+    }
+
+    public void mainUI() {
+        SwingUtilities.invokeLater(() -> {
+            new BankUI();
+        });
     }
 }
-
-
-
-
