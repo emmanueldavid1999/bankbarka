e6de22696d8126f958caed33dc7ae0e360158774
estarrMill <133377945+estarrMill@users.noreply.github.com>
2023-05-26 15:26:56 +0100

    Update savings_account.java

--- BankUI.java/src/UI/savings_account.java
+++ BankUI.java/src/UI/savings_account.java
@@ -1,51 +1,73 @@
-package UI;
 
-public class savings_account {
-    private double balance;
-    private double maxWithdrawal;
 
-    public savings_account(double initialBalance, double maxWithdrawal) {
-        this.balance = initialBalance;
-        this.maxWithdrawal = maxWithdrawal;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SavingsAccount {
+    private final int DEFAULT_PIN = 5555;
+
+    private JFrame savingsFrame;
+    private JLabel savingsLabel;
+    private JButton savingsWithdrawalButton;
+    private JButton savingsDepositButton;
+    private JTextField savingsAmountField;
+    private JTextField savingsPinField;
+
+    public SavingsAccount() {
+        initializeUI();
+        addEventListeners();
     }
 
-    public void withdraw(double amount) {
-        if (amount <= balance && amount <= maxWithdrawal) {
-            balance -= amount;
-            System.out.println("Withdrawal successful. Remaining balance: " + balance);
-        } else if (amount > maxWithdrawal) {
-            System.out.println("Withdrawal amount exceeds maximum withdrawal limit.");
-        } else {
-            System.out.println("Insufficient balance.");
-        }
+    private void initializeUI() {
+        savingsFrame = new JFrame("JD SAVINGS ACCOUNT");
+        savingsLabel = new JLabel("THANK YOU FOR CHOOSING JD SAVINGS ACCOUNT");
+        savingsWithdrawalButton = new JButton("WITHDRAWAL");
+        savingsDepositButton = new JButton("DEPOSIT");
+        savingsAmountField = new JTextField("        ");
+        savingsPinField = new JTextField("         ");
+
+        savingsFrame.setLayout(new FlowLayout());
+        savingsFrame.add(savingsLabel);
+        savingsFrame.add(savingsWithdrawalButton);
+        savingsFrame.add(savingsDepositButton);
+        savingsFrame.add(savingsAmountField);
+        savingsFrame.add(savingsPinField);
+        savingsFrame.setSize(300, 150);
     }
 
-    public void deposit(double amount) {
-        balance += amount;
-        System.out.println("Deposit successful. Current balance: " + balance);
+    private void addEventListeners() {
+        savingsWithdrawalButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int pin = Integer.parseInt(savingsPinField.getText());
+                if (pin == DEFAULT_PIN) {
+                    double amount = Double.parseDouble(savingsAmountField.getText());
+                    // Perform withdrawal logic for savings account
+                    JOptionPane.showMessageDialog(null, "Withdrawal successful.");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Invalid PIN.");
+                }
+            }
+        });
+
+        savingsDepositButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int pin = Integer.parseInt(savingsPinField.getText());
+                if (pin == DEFAULT_PIN) {
+                    double amount = Double.parseDouble(savingsAmountField.getText());
+                    // Perform deposit logic for savings account
+                    JOptionPane.showMessageDialog(null, "Deposit successful.");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Invalid PIN.");
+                }
+            }
+        });
     }
 
-    public double getBalance() {
-        return balance;
-    }
-
-    public double getMaxWithdrawal() {
-        return maxWithdrawal;
-    }
-
-    public static void main(String[] args) {
-        savings_account account = new savings_account(100000, 10000);
-        System.out.println("Initial balance: " + account.getBalance());
-
-        // Example withdrawals
-        account.withdraw(5000);  // Valid withdrawal
-        account.withdraw(15000); // Exceeds maximum withdrawal limit
-        account.withdraw(100000); // Exceeds available balance
-
-        // Example deposit
-        account.deposit(5000); // Valid deposit
-        account.deposit(100000); // Valid deposit, no limit
-
-        System.out.println("Final balance: " + account.getBalance());
+    public void show() {
+        savingsFrame.setVisible(true);
     }
 }
